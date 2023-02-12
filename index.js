141ded42dea6f279fa678b655af91fab2eaa2d9e
kudzanai murefu <kudzmurefu@gmail.com>
2023-02-11 21:24:45 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.send('Hello Welcome')
+})
+app.get('/ec2', (req, res) => {
+  return res.send('Ec2 Instance - Working Fine (Created by Kudz Murefu)')
+})
